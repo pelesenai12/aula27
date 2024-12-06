15d685125895c10f8eb60adf14c967c99f78c788
thallyson pele <thallyson.pinheiro@aluno.senai.br>
2024-12-06 09:49:40 -0500

    alteracoes feitas dia 06/12/2024

--- atividade.py
+++ atividade.py
@@ -1,37 +1,44 @@
-    #ler entradas do usuarios
-cont = 0#variavel controla a repeticao
-alunos = []
-escolha_usuario= int(input("degite a quantidade de alunos que voce deseja cadastra:"))#variavel que guarda quantas vezes o codigo vai rodar
-while cont < escolha_usuario:
+   #LER ENTRADAS DO USUÁRIO
+alunos=[]#
+n1 = n2 = n3 = n4 = media = 0
+nome=""
+faltas=0
+situacao=""
+while True:
+    escolha_menu = int(input("Escolha uma das opções do menu:"))#Variável que guarda a escolha do usuário
+    if escolha_menu == 1:#se a escolha para realizar
+        situacao = ""
+        cont = 0#variavel que controla a repetição
+        escolha_usuario= int(input("Digite a quantidade de alunos que você quer cadastrar:"))#variavel que guarda quantas vezes o código vai rodar
+        while cont < escolha_usuario:
+            nome= input("Digite o nome do aluno:")#ARMAZENA o nome do aluno
+            n1=float(input("Digite a 1° nota:"))#as 4 notas do aluno
+            n2=float(input("Digite a 2° nota:"))
+            n3=float(input("Digite a 3° nota:"))
+            n4=float(input("Digite a 4° nota:"))
 
-    nome = input()#armazenar o nome do aluno
-    nota1 = float(input())#ler 4 notas do aluno
-    nota2 = float(input())
-    nota3 = float(input())
-    nota4 = float(input())
-
-    faltas = int(input(f"degite a quantidade de faltas o {nome} tem:"))#calculo da media
-    #calculo de
-    media = (nota1+nota2+nota3+nota4)/4
-    print(media)
-    #logica da situação
-    if faltas >31:
-        situacao = "reprovado por falta"
-    elif media >=8:
-        situacao = "aprovado"
-    elif media >=5:#recuperação
-        recuperacao = float(input())#ler a nota da recuperação
-        if recuperacao >= (10-media):
-            situacao = "aprovado na recuperação"
-        else:
-            situacao = "reprovado na recuperação"
-    else:
-        situacao = "reprovado por media"
-    #enviar os dados do aluno para a lista (alunos)  
-    alunos.append([nome, faltas, media, situacao])
-    cont =+ 1
-#relatorio
-print(alunos)
-
+            faltas= int(input(f"Digite a quantidade de faltas o {nome} tem:"))
+            #calculo da média
+            media= (n1+n2+n3+n4)/4
+            print(media)
+            if media >=8:
+                situacao="aprovado acima da média"
+            elif media >=5:
+                recuperacao= float(input("digite a nota da recuperação:"))
+                if recuperacao >=(10-media):
+                    situacao= "aprovado na recuperação!"
+                else:
+                    situacao= "reprovaddo na recuperacao"
+            else:
+                situacao= "reprovado pela média!"
+            #enviar os dados do aluno para a lista alunos
+            alunos.append([nome, faltas, media, situacao])
+            cont =+ 1
+            #RELATORIO
         
-
+    elif escolha_menu == 2: #relatorio
+        alunos.append([nome, faltas, media, situacao])
+        cont += 1
+        print(alunos)
+    elif escolha_menu == 3:# se o usuario escolheu encerrar 
+        break #quebra a execucao do enquanto
