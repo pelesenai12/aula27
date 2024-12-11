c0140ae54e7f748370b1d0962400f95598a13797
thallyson pele <thallyson.pinheiro@aluno.senai.br>
2024-12-11 10:08:03 -0500

    salvar novas declaracoes

--- funcao.py
+++ funcao.py
@@ -1,6 +1,15 @@
+#declaracao as funcao
+def saudacoes (hora_do_dia):#exibir a saudacao correspondente a hora
+    #bom dia
+    if (hora_do_dia >=0) and (hora_do_dia <=12):
+        print("bom dia!!!!")  
+        #condicao para dar boa noite
+    elif(hora_do_dia >=13) and (hora_do_dia <=18):
+        print("boa tarde!")
+    else:
+        print("boa noite!")
+#fora da funcao 
+#peco para o usuarioi dizer a hora atual
+resposta=int(input("digite que horas sao:\n"))
 
-
-def boa_noite():
-    print("boa noite!")
-
-boa_noite()
+saudacoes(resposta)
